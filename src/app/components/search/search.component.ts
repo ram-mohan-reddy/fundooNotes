a799301bd6525e233e6a57f46e8cb0e8162483a2
ram <ram.mohan10595@gmail.com>
2018-11-03 19:53:15 +0530

    fundooNotes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -11,26 +11,22 @@ export class SearchComponent implements OnInit {
 
   list;
   totalNotes: any = [];
-  constructor(private notesService : GetNotesService,public dataService: DataSharingService) { }
-searchText : string;
+  searchText : string;
+  constructor(private notesService : GetNotesService,public dataService: DataSharingService) {this.dataService.currentMessage.subscribe(message => {
+    this.searchText = message;
+    this.notesService.getNotes()
+    .subscribe(data => {
+      console.log(data);
+      this.notesCollection(data) 
+    });
+    error => console.log('Error ', error);
+  }) }
+
   ngOnInit() {
-    this.dataService.currentMessage.subscribe(message => {
-      console.log(message);
-      
-      this.searchText = message;
-      this.notesService.getNotes()
-      .subscribe(data => {
-        console.log(data);
-        this.notesCollection(data) 
-      });
-      error => console.log('Error ', error);
-    })
-   
   }
 
   notesCollection(data) {
     this.list = [];
-
     for (let index = 0; index < data['data'].data.length; index++) {
       if (data['data'].data[index].isDeleted == false) {
         this.list.push(data['data'].data[index])
--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -5,9 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  // transform(value: any, args?: any): any {
-  //   return null;
-  // }
 
   transform(value: any, args?: any): any {
 
