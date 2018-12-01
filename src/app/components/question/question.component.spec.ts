d5bb9729912a4557ea07967f25acd4ce100a34f2
ram <ram.mohan10595@gmail.com>
2018-12-01 19:27:53 +0530

    fundooNotes

--- src/app/components/question/question.component.spec.ts
+++ src/app/components/question/question.component.spec.ts
@@ -22,4 +22,49 @@ describe('QuestionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return true when there is a like by user', () => {
+    let likeArray = [{
+      like : true,
+      userId:localStorage.getItem('userId')
+    }]
+    let question = {
+      like : likeArray
+    }
+    expect(component.checkLike(question)).toBeTruthy();
+  });
+
+  it('should return false when there is a no like by user', () => {
+    let likeArray = [{
+      like : true,
+      userId:"5bc0287b5414a900407e8e59"
+    }]
+    let question = {
+      like : likeArray
+    }
+    expect(component.checkLike(question)).toBeFalsy();
+  });
+  
+  it('should return true when there is a rating given by user', () => {
+    let rateArray = [{
+      rate : 4,
+      userId:localStorage.getItem('userId')
+    }]
+    let question = {
+      rate : rateArray
+    }
+    expect(component.checkRate(question)).toBeTruthy();
+  });
+
+  it('should return false when there is a no rating given by user', () => {
+    let rateArray = [{
+      rate : 3,
+      userId:"5bc0287b5414a900407e8e59"
+    }]
+    let question = {
+      rate : rateArray
+    }
+    expect(component.checkRate(question)).toBeFalsy();
+  });
 });
+
--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GetNotesService } from '../../core/services/notes/get-notes.service';
 import { ActivatedRoute } from '@angular/router';
 import { QuestionService } from '../../core/services/questionService/question.service';
-import{environment} from '../../../environments/environment'
+import { environment } from '../../../environments/environment'
 
 @Component({
   selector: 'app-question',
@@ -11,57 +11,59 @@ import{environment} from '../../../environments/environment'
 })
 export class QuestionComponent implements OnInit {
 
-  constructor(private notesService:GetNotesService,private route: ActivatedRoute,
-  private questionService:QuestionService) { }
-  note:any;
-  title:string ='';
-  description:string ='';
-  checkList=[];
-  newQuestion:string='';
-  questionAndAnswers=[];
+  constructor(private notesService: GetNotesService, private route: ActivatedRoute,
+    private questionService: QuestionService) { }
+  note: any;
+  title: string = '';
+  description: string = '';
+  checkList = [];
+  newQuestion: string = '';
+  questionAndAnswers = [];
   questionAsked: string = '';
-  replyAnswer: string ='';
-  showReply:string = '';
-  color:string='';
-  private savedUrl:string='';
-  private url:string='';
-  private firstName='';
-  private lastName='';
+  replyAnswer: string = '';
+  showReply: string = '';
+  color: string = '';
+  private savedUrl: string = '';
+  private url: string = '';
+  private firstName = '';
+  private lastName = '';
   private server_url = environment.baseUrl;
-  starList: boolean[] = [true,true,true,true,true]; 
   private user: string = '';
   panelOpenState = false;
-  private rate : number;
-  private rateAverage : number;
+  private rate: number;
+  private rateAverage: number;
+  private noteId: string = '';
+  private replyShow: boolean = false;
+  private showArrow:string='';
+  private showArrowAnswers:string='';
+  private showArrowNestedAnswers:string='';
+  
   ngOnInit() {
-   
+
     this.route.params.subscribe(params => {
       if (params) {
+        this.noteId = params.id;
         this.getNoteDetails(params.id);
       }
     });
-this.user = localStorage.getItem('userId');
+    this.user = localStorage.getItem('userId');
   }
   getNoteDetails(noteId) {
-    this.notesService.getLabelData('api/notes/getNotesDetail/'+noteId)
-    .subscribe(data => {
-      // console.log(data['data'].data[0]);
-      this.note = data['data'].data[0];
-      console.log(this.note);
-      this.title = this.note.title;
-      this.description = this.note.description;
-      this.color = this.note.color;
-      this.checkList = this.note.noteCheckLists;
-      this.questionAndAnswers = this.note.questionAndAnswerNotes;
-      if (this.questionAndAnswers.length != 0) {
-        this.questionAsked=this.questionAndAnswers[0];
-        this.firstName= this.note.questionAndAnswerNotes[0].user.firstName;
-        this.lastName= this.note.questionAndAnswerNotes[0].user.lastName;
-        // this.savedUrl = this.note.questionAndAnswerNotes[0].user.imageUrl;
-      }
-     
-      // this.url = this.server_url + this.savedUrl;
-    });
+    this.notesService.getLabelData('api/notes/getNotesDetail/' + noteId)
+      .subscribe(data => {
+        this.note = data['data'].data[0];
+        console.log(this.note);
+        this.title = this.note.title;
+        this.description = this.note.description;
+        this.color = this.note.color;
+        this.checkList = this.note.noteCheckLists;
+        this.questionAndAnswers = this.note.questionAndAnswerNotes;
+        if (this.questionAndAnswers.length != 0) {
+          this.questionAsked = this.questionAndAnswers[0];
+          this.firstName = this.note.questionAndAnswerNotes[0].user.firstName;
+          this.lastName = this.note.questionAndAnswerNotes[0].user.lastName;
+        }
+      });
   }
   imageUrl(question) {
     this.savedUrl = question.user.imageUrl;
@@ -69,27 +71,28 @@ this.user = localStorage.getItem('userId');
     return true;
   }
   onEnter(newQuestion) {
-    console.log(newQuestion);
     let data = {
       "message": this.newQuestion,
       "notesId": this.note.id
     }
     this.newQuestion = '';
     this.questionService.addQuestion(data)
-    .subscribe(data=>{
-      console.log(data);
-    });
+      .subscribe(data => {
+        this.getNoteDetails(this.noteId);
+      });
   }
 
   reply(parentId) {
-    let data = {
-      "message": this.replyAnswer
+    if (this.replyAnswer != '') {
+      let data = {
+        "message": this.replyAnswer
+      }
+      this.replyAnswer = '';
+      this.questionService.reply(parentId, data)
+        .subscribe(data => {
+          this.getNoteDetails(this.noteId);
+        })
     }
-    this.replyAnswer = '';
-    this.questionService.reply(parentId, data)
-      .subscribe(data => {
-        console.log(data);
-      })
   }
 
   like(parentId) {
@@ -98,24 +101,21 @@ this.user = localStorage.getItem('userId');
     }
     this.questionService.like(parentId, data)
       .subscribe(data => {
-        console.log(data);
+        this.getNoteDetails(this.noteId);
       })
   }
 
-  rateQuestion(value,parentId) {
-    console.log(value);
-    console.log(parentId);
+  rateQuestion(value, parentId) {
     let data = {
       "rate": value
     }
     this.questionService.rate(parentId, data)
       .subscribe(data => {
-        console.log(data);
+        this.getNoteDetails(this.noteId);
       })
   }
 
   checkLike(question) {
-
     for (let index = 0; index < question.like.length; index++) {
       if (question.like[index].userId == this.user) {
         return true
@@ -127,7 +127,7 @@ this.user = localStorage.getItem('userId');
   checkRate(question) {
     this.rateAverage = 0;
     for (let index = 0; index < question.rate.length; index++) {
-      this.rateAverage = (this.rateAverage + question.rate[index].rate)/question.rate.length;
+      this.rateAverage = (this.rateAverage + question.rate[index].rate) / question.rate.length;
       if (question.rate[index].userId == this.user) {
         this.rate = question.rate[index].rate;
         return true;
@@ -136,6 +136,16 @@ this.user = localStorage.getItem('userId');
     this.rate = 0;
     return false;
   }
+private replies:number;
+  replyCheck(questionAsked){
+    this.replies = 0;
 
+    for (let index = 0; index < this.questionAndAnswers.length; index++) {
+      if (questionAsked.id == this.questionAndAnswers[index].parentId) {
+        this.replies++;
+      }
+    }
 
+    return this.replies;
+  }
 }
